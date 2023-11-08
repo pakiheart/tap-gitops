529bb2789051cde265933fa2d75b663f003ba1d4
Rehan Altaf <raltaf@raltafFMD6R.vmware.com>
2023-11-08 10:32:33 -0700

    updatating to 1.7

--- clusters/cg-build/tanzu-sync/scripts/configure-secrets.sh
+++ clusters/cg-build/tanzu-sync/scripts/configure-secrets.sh
@@ -9,7 +9,8 @@ $0 :: configure Tanzu Sync for use with External Secrets Operator (ESO)
 Required Environment Variables:
 - AWS_ACCOUNT_ID -- Account ID owning the named IAM Policy
 - AWS_REGION -- region from where to fetch Secrets Manager secrets
-- EKS_CLUSTER_NAME -- cluster on which TAP is being installed
+- EKS_CLUSTER_NAME -- (deprecated) cluster on which TAP is being installed
+- CLUSTER_NAME -- cluster on which TAP is being installed
 
 Optional:
 - IAM_ROLE_NAME_FOR_TANZU_SYNC -- name of IAM Role (to be created) which will be used to access Tanzu Sync secrets
@@ -18,17 +19,19 @@ Optional:
 EOF
 }
 
+CLUSTER_NAME="${CLUSTER_NAME:-$EKS_CLUSTER_NAME}"
+
 error_msg="Expected env var to be set, but was not."
 : "${AWS_ACCOUNT_ID?$error_msg}"
 : "${AWS_REGION?$error_msg}"
-: "${EKS_CLUSTER_NAME?$error_msg}"
+: "${CLUSTER_NAME?$error_msg}"
 
-IAM_ROLE_NAME_FOR_TANZU_SYNC=${IAM_ROLE_NAME_FOR_TANZU_SYNC:-${EKS_CLUSTER_NAME}--tanzu-sync-secrets}
-IAM_ROLE_NAME_FOR_TAP=${IAM_ROLE_NAME_FOR_TAP:-${EKS_CLUSTER_NAME}--tap-install-secrets}
+IAM_ROLE_NAME_FOR_TANZU_SYNC=${IAM_ROLE_NAME_FOR_TANZU_SYNC:-${CLUSTER_NAME}--tanzu-sync-secrets}
+IAM_ROLE_NAME_FOR_TAP=${IAM_ROLE_NAME_FOR_TAP:-${CLUSTER_NAME}--tap-install-secrets}
 
 # configure
 # (see: tanzu-sync/app/config/.tanzu-managed/schema.yaml)
-ts_values_path=tanzu-sync/app/values/tanzu-sync-eso.yaml
+ts_values_path=tanzu-sync/app/values/tanzu-sync-aws-secrets-manager-values.yaml
 cat > ${ts_values_path} << EOF
 ---
 secrets:
@@ -38,21 +41,16 @@ secrets:
       tanzu_sync_secrets:
         role_arn: arn:aws:iam::${AWS_ACCOUNT_ID}:role/${IAM_ROLE_NAME_FOR_TANZU_SYNC}
     remote_refs:
-      sync_git_ssh:
-        ssh_private_key:
-          key: dev/${EKS_CLUSTER_NAME}/tanzu-sync/sync-git-ssh
-          property: ssh-privatekey
-        ssh_known_hosts:
-          key: dev/${EKS_CLUSTER_NAME}/tanzu-sync/sync-git-ssh
-          property: ssh-knownhosts
+      sync_git:
+        # TODO: Fill in your configuration for ssh or basic auth here (see tanzu-sync/app/config/.tanzu-managed/schema--eso.yaml)
       install_registry_dockerconfig:
         dockerconfigjson:
-          key: dev/${EKS_CLUSTER_NAME}/tanzu-sync/install-registry-dockerconfig
+          key: dev/${CLUSTER_NAME}/tanzu-sync/install-registry-dockerconfig
 EOF
 
 echo "wrote ESO configuration for Tanzu Sync to: ${ts_values_path}"
 
-tap_install_values_path=cluster-config/values/tap-install-eso-values.yaml
+tap_install_values_path=cluster-config/values/tap-install-aws-secrets-manager-values.yaml
 cat > ${tap_install_values_path} << EOF
 ---
 tap_install:
@@ -65,7 +63,9 @@ tap_install:
       remote_refs:
         tap_sensitive_values:
           sensitive_tap_values_yaml:
-            key: dev/${EKS_CLUSTER_NAME}/tap/sensitive-values.yaml
+            key: dev/${CLUSTER_NAME}/tap/sensitive-values.yaml
 EOF
 
-echo "wrote ESO configuration for TAP install to: ${tap_install_values_path}"
+echo "wrote AWS Secrets Manager configuration for TAP install to: ${tap_install_values_path}"
+echo ""
+echo "Please edit '${ts_values_path}' filling in values for each 'TODO' comment"
--- clusters/cg-build/tanzu-sync/scripts/deploy.sh
+++ clusters/cg-build/tanzu-sync/scripts/deploy.sh
@@ -5,6 +5,5 @@ set -o errexit -o nounset -o pipefail
 kapp deploy -a tanzu-sync \
   -f <(ytt -f tanzu-sync/app/config \
            -f cluster-config/config/tap-install/.tanzu-managed/version.yaml \
-           --data-values-file tanzu-sync/app/values/ \
-           --data-values-file sensitive-values.sh.stdout=<(tanzu-sync/scripts/sensitive-values.sh) \
+           --data-values-file tanzu-sync/app/values/
       ) $@
